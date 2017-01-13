239609da809149ede6ad98b109dc2d0ae3aea901
Patrick Borrelli <patrickborrelli@gmail.com>
2017-01-13 04:26:17 -0500

    addition of team member model in place of separate player, coach,parent

--- models/teamMember.js
+++ models/teamMember.js
@@ -1,21 +1,27 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Team = require('./team.js');
+var Role = require('./role.js');
 var User = require('./user.js');
 
-var TeamCoach = new Schema({
+var TeamMember = new Schema({
     team: {
         type: Schema.Types.ObjectId,
         ref: 'Team',
         required: true
     },
-    coach: {
+    member: {
         type: Schema.Types.ObjectId,
         ref: 'User',
+        required: true
+    },
+    role: {
+        type: Schema.Types.ObjectId,
+        ref: 'Role',
         required: true
     }
 }, {
     timestamps: true
 });
 
-module.exports = mongoose.model('TeamCoach', TeamCoach);
+module.exports = mongoose.model('TeamMember', TeamMember);

6add80c8e5601d00c713f3fbfd0930301d250123
Patrick Borrelli <patrickborrelli@gmail.com>
2017-01-18 17:48:11 -0500

    added path POST /facilities/addField/:facilityId/:fieldId

--- routes/facilities.js
+++ routes/facilities.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Facility = require('../models/facility');
+var Field = require('../models/field');
+var async = require('async');
 var Verify = require('./verify');
 
 var router = express.Router();
@@ -69,5 +71,52 @@ router.route('/:facilityId')
             res.json("Successfully removed facility " + facility.name);
     });
 });
+
+//#################################################################################################
+//#################################################################################################
+router.route('/addField/:facilityId/:fieldId')
+
+///POST add a field to a facility by ID
+.post(Verify.verifyOrdinaryUser, function(req, res, next) {
+    async.waterfall(
+        [
+            function(callback) {
+                Facility.findById(req.params.facilityId)
+                    .populate('club_affiliation')                    
+                    .populate('fields')
+                    .exec(function(err, facility) {
+                        if(err) throw err;
+                        callback(null, facility);
+                });
+                
+            },
+            function(facility, callback) {
+                //add the facility id to the field and save the field:
+                Field.findByIdAndUpdate(req.params.fieldId, {$set: {facility: facility._id}}, {new: true}) 
+                    .exec(function(err, field) {
+                        if(err) throw err;
+                        callback(null, facility, field);
+                });                             
+            }, 
+            function(facility, field, callback) {
+                console.log("attempting to put field: " + field);
+                console.log("into facility: " + facility);
+                //now, push the field into the facility and save it.
+                facility.fields.push(field);
+                facility.save()
+                    .then(function(facility) {
+                    console.log("Successfully saved " + facility);
+                    callback(null, facility);
+                },function(errResponse) {
+                    console.log("Here is where it all falls down " + errResponse);
+                });
+            }
+        ],
+        function(err, facility) {
+            if(err) return next(err);        
+            res.json(facility);
+        }
+    )
+})
 
 module.exports = router;

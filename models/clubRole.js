ab0722768d47ef35c8d760f88409ac9ca1b9ce3d
Patrick Borrelli <patrickborrelli@gmail.com>
2017-01-27 01:09:47 -0500

    CLUB_ROLE added so properly model the user/club/role relationship

---
+++ models/clubRole.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Club = require('./club.js');
+var Role = require('./role.js');
+var User = require('./user.js');
+
+var ClubRole = new Schema({
+    club: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'Club'
+    },
+    role: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'Role'
+    },
+    added_by: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('ClubRole', ClubRole);

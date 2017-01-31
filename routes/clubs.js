ac99c65ed186a6d2f6ae59a9bbe2096219414adc
Patrick Borrelli <patrickborrelli@gmail.com>
2017-01-30 22:21:04 -0500

    added getClubMembers path to CLUBS

--- routes/clubs.js
+++ routes/clubs.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Club = require('../models/club');
+var ClubRole = require('../models/clubRole');
 var Verify = require('./verify');
 
 var router = express.Router();
@@ -73,6 +74,27 @@ router.route('/:clubId')
             club.remove();
             res.json("Successfully removed " + club.name);
     });
+});
+
+
+//#################################################################################################
+//#################################################################################################
+router.route('/getClubMembers/:clubId')
+
+///GET all club members by club ID
+.get(Verify.verifyOrdinaryUser, function(req, res, next) {
+    
+    ClubRole.find({club: req.params.clubId})
+        .populate('member')
+        .exec(function(err, userRoles) {
+            if(err) return next(err);
+            var users = [];
+            
+            for(var i = 0; i < userRoles.length; i++) {
+                users.push(userRoles[i].member);
+            }
+            res.json(users);
+    });   
 });
 
 

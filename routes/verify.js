06e15c30529573ad34ff91313b13e1c3f08b0b47
Patrick Borrelli <patrickborrelli@gmail.com>
2017-03-21 15:48:44 -0400

    remove unused require statements

--- routes/verify.js
+++ routes/verify.js
@@ -1,10 +1,6 @@
 var User = require('../models/user');
 var jwt = require('jsonwebtoken'); //used to create, sign and verify tokens
 var config = require('../config.js');
-//var Blog = require('../models/blog');
-//var Entry = require('../models/entry');
-//var Comment = require('../models/comment');
-//var Post = require('../models/post');
 
 exports.getToken = function(user) {
     return jwt.sign(user, config.secretKey, {

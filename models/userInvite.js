6a46d87c7acfb17beeb170993a572c7683727658
Patrick Borrelli <patrickborrelli@gmail.com>
2017-03-21 23:32:01 -0400

    typo correction

--- models/userInvite.js
+++ models/userInvite.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Role = require('./role.js');
-var Club = rquire('./club.js');
+var Club = require('./club.js');
 
 var UserInvite = new Schema({
     invite_key: {

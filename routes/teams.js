b5af3f50c5f235db23053c3bb228793aaaf39b0f
Patrick Borrelli <patrickborrelli@gmail.com>
2017-04-05 12:12:06 -0400

    added route /getAllTeamsWIthLeagues/teams

--- routes/teams.js
+++ routes/teams.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var async = require('async');
 var Team = require('../models/team');
+var LeagueTeam = require("../models/leagueTeam")
 var Verify = require('./verify');
 
 var router = express.Router();
@@ -78,6 +80,63 @@ router.route('/:teamId')
             team.remove();
             res.json("Successfully removed team " + fullName);
     });
+});
+
+//#################################################################################################
+//#################################################################################################
+router.route('/getTeamWithLeagues/teams')
+
+///GET retrieve all teams along with all leagues the team participates in
+.get(Verify.verifyOrdinaryUser, function(req, res, next) {
+    
+    var newTeams = [];
+
+    async.waterfall(
+        [
+            function(callback) {
+                Team.find(req.query)
+                    .populate('gender')
+                    .populate('age_group')
+                    .populate('club')
+                    .exec(function(err, teams) {
+                        if(err) throw err;
+                        callback(null, teams);
+                });  
+            },
+            function(teams, callback) {     
+                async.forEach(teams, function(team, callback) { 
+                    var myleagues = [];
+                    LeagueTeam.find({team: team._id}) 
+                        .populate('team')
+                        .populate('league')
+                        .exec(function(err, leagueTeams) {
+                            if(err) throw err;
+                            console.log("Found " + leagueTeams.length + " leagues for team " + team.name);
+                            console.log(leagueTeams);
+                            for(var i = 0; i < leagueTeams.length; i++) {
+                                myleagues.push(leagueTeams[i].league);
+                            }
+                            var newTeam = {
+                                _id: team._id,
+                                name: team.name,
+                                gender: team.gender,
+                                age_group: team.age_group,
+                                club: team.club,
+                                leagues: myleagues
+                            }
+                            newTeams.push(newTeam);
+                            callback();                            
+                    });                
+                }, function(err) {
+                    if (err) return next(err);
+                    callback(null, newTeams);
+                });
+            }
+        ],
+        function(err, newTeams) {
+            res.json(newTeams);
+        }
+    ) 
 });
 
 module.exports = router;

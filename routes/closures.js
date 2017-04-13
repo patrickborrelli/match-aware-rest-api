df00e0006ad9b62f7a9b131f6c6073cf2f911379
Patrick Borrelli <patrickborrelli@gmail.com>
2017-04-12 23:41:01 -0400

    addition of openFieldOnly path to CLOSURES

--- routes/closures.js
+++ routes/closures.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var async = require('async');
 var Closure = require('../models/closure');
+var Field = require('../models/field');
 var Verify = require('./verify');
 
 var router = express.Router();
@@ -74,6 +76,99 @@ router.route('/:closureId')
             res.json("Successfully removed " + closure);
     });
 });
+
+//#################################################################################################
+//#################################################################################################
+router.route('/openFieldOnly/:fieldId/:closureId')
+
+///POST replacement closure for field by ID
+.post(Verify.verifyOrdinaryUser, function(req, res, next) {
+    /**
+     * In the case where a closure applies to both a field and the facility as a whole, but the intent
+     * is to open just the field, this path provides a mechanism to do so.
+     *      1 - create a copy of the closure
+     *      2 - change its end time to the current time
+     *      3 - save the new closure
+     *      4 - add the new closure to the field's closures array
+     *      5 - remove the original closure from the field's closures array
+     *      6 - save the field.
+     */
+    
+    async.waterfall(
+        [
+            function(callback) {
+                //first retrieve existing closure:
+                Closure.findById(req.params.closureId)
+                    .exec(function(err, closure) {
+                        if(err) throw err;
+                        callback(null, closure);
+                });
+            },
+            function(oldClosure, callback) {
+                if(oldClosure == null) {
+                    var err = new Error('Closure with matching ID not found.');
+                    err.status = 404;
+                    return next(err);
+                }
+                
+                console.log("Making a copy of existing closure:");
+                console.log(oldClosure);
+                var currentTime = new Date().getTime();
+                
+                Closure.create({type: oldClosure.type, start: oldClosure.start, end: currentTime, message: oldClosure.message}, function(err, newClosure) {
+                    if(err) return next(err);
+                    console.log("New closure created " + newClosure);
+                    callback(null, newClosure, oldClosure);
+                });
+            },
+            function(newClosure, oldClosure, callback) {
+                Field.findById(req.params.fieldId)
+                    .exec(function(err, field) {
+                        if(err) throw err;
+                        if(field == null) {
+                            var err = new Error('Field with matching ID not found.');
+                            err.status = 404;
+                            return next(err);
+                        }
+                        callback(null, newClosure, oldClosure, field);
+                    });                             
+            },
+            function(newClosure, oldClosure, field, callback) {
+                var fieldClosures = field.closures;
+                var index = fieldClosures.indexOf(oldClosure._id);
+                
+                console.log("Checking for index " + index + " in:");
+                console.log(fieldClosures);
+                
+                if(index > -1) {
+                    fieldClosures.splice(index, 1);
+                }
+                fieldClosures.push(newClosure._id);
+                console.log("TEMP field closures now contains:");
+                console.log(fieldClosures);
+                
+                field.closures = fieldClosures;
+                
+                console.log("Field closures now contains:");
+                console.log(field.closures);
+                
+                field.save(function(err, savedField) {
+                    if(err) return next(err);
+                    console.log("Updated field:");
+                    console.log(savedField);
+                    callback(null, savedField);
+                });              
+            }
+        ],
+        function(err, savedField) {
+            if(err) return next(err);
+            res.json(savedField);
+        }   
+    )  
+    
+    
+    
+})
 
 
 module.exports = router;
